de12f062b2ccf48429a45e604d66e2c107ba96e3
ShrikantJawla <shrikantjawla@gmail.com>
2023-11-19 02:24:25 +0530

    font size increased.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-[100vw] min-h-[100vh] max-w-[1440px] dark:bg-[#000000] m-auto flex justify-center items-center px-[16px] md:px-[40px] lg:px-[80px] xl:px-[170px] py-[10px] md:py-[24px] border border-red-400">
+    <main className="w-[100vw] min-h-[100vh] max-w-[1640px] dark:bg-[#000000] m-auto flex justify-center items-center px-[16px] md:px-[40px] lg:px-[80px] xl:px-[140px] py-[10px] md:py-[24px] border border-red-400">
       <section className="w-full min-h-[100vh]">
         <Navbar />
       </section>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div className="w-full h-[50px] md:h-[60px] rounded-[15px] bg-[--nav_bg] dark:bg-[#1A1A1A] flex justify-between items-center px-[12px] md:px-[20px] lg:px-[30px] xl:px-[53px]">
+    <div className="w-full h-[55px] md:h-[70px] sticky top-[10px] rounded-[15px] bg-[--nav_bg] dark:bg-[#1A1A1A] flex justify-between items-center px-[12px] md:px-[20px] lg:px-[30px] xl:px-[53px]">
       <a href="#_home">
         <Image
           src={HomeIcon}
@@ -21,7 +21,7 @@ const Navbar = (props: Props) => {
       {["Skills", "Projects", "About", "Contact", "Resume/CV"].map(
         (ele, ind) => (
           <a key={ind} className="group hidden lg:flex" href="#_skills">
-            <p className="group text-[18px] font-[500] text-[#55505B] dark:text-white hover:scale-125 hover:text-[--theme_purple] hover:dark:text-[--theme_purple] transition-scale duration-300 transition-ease relative before:content-[''] before:absolute before:bottom-[-2px] before:height-[6px] before:w-[0] group-hover:before:w-full group-hover:before:border before:border-[--theme_purple] dark:before:border-white before:rounded-[50px] before:transition-width before:duration-[0.9s] before:transition-ease">
+            <p className="group text-[19px] font-[600] text-[#55505B] dark:text-white hover:scale-125 hover:text-[--theme_purple] hover:dark:text-[--theme_purple] transition-scale duration-300 transition-ease relative before:content-[''] before:absolute before:bottom-[-2px] before:height-[6px] before:w-[0] group-hover:before:w-full group-hover:before:border before:border-[--theme_purple] dark:before:border-white before:rounded-[50px] before:transition-width before:duration-[0.9s] before:transition-ease">
               {ele}
             </p>
           </a>
