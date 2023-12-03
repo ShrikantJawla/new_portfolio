f530d0e19d342ae6db7c8721dd1ce9f8d5800f70
ShrikantJawla <shrikantjawla@gmail.com>
2023-12-03 23:33:19 +0530

    meta data updated.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,23 @@ import NextThemeProvider from "@/components/Provider";
 export const metadata: Metadata = {
   title: "Shrikant Jawla | Full Stack Web Developer",
   description: "Portfolio website of Shrikant Jawla | Full Stack Web Developer",
+  icons: {
+    icon: "/logo.png",
+  },
+  authors: [{ name: "Shrikant Jawla" }],
+  openGraph: {
+    type: "website",
+    url: "/",
+    title: "Shrikant Jawla | Full Stack Web Developer",
+    description:
+      "Portfolio website of Shrikant Jawla | Full Stack Web Developer",
+    siteName: "Shrikant Jawla | Full Stack Web Developer",
+    images: [
+      {
+        url: "/logo.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
